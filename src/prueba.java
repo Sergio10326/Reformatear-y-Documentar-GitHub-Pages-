8395796e0366f872f7c5559bfee7e7d8b38c042a
sergio10326 <sdelaiglesialorenzo@gmail.com>
2023-09-29 09:16:47 +0200

    2nd try

---
+++ src/prueba.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package src;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sergio10326
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                float base,altura,area;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce la base:");
+        base = sc.nextFloat();
+        System.out.println("introduce la altura:");
+        altura = sc.nextFloat();
+        area = (base*altura)/2;
+            System.out.println("area= "+ area);
+    }
+    
+}
+
+
+
